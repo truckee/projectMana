11b2c6429d73e62da6a1f5ee9b65860b66879791
George <George@GWB-laptop>
2016-08-09 09:44:10 -0700

    remove superfluous use statement

--- src/Truckee/ProjectmanaBundle/Controller/ContactController.php
+++ src/Truckee/ProjectmanaBundle/Controller/ContactController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Truckee\ProjectmanaBundle\Entity\Contact;
 use Truckee\ProjectmanaBundle\Entity\Center;
 use Truckee\ProjectmanaBundle\Form\ContactType;
