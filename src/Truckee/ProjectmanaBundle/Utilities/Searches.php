c4ff31fb421016e1598daa5d141ffd1f93d65597
George <George@GWB-laptop>
2016-09-03 10:33:31 -0700

    restore getHeadsFYToDate()

--- src/Truckee/ProjectmanaBundle/Utilities/Searches.php
+++ src/Truckee/ProjectmanaBundle/Utilities/Searches.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Truckee\Projectmana package.
  *
@@ -20,28 +19,29 @@ use Doctrine\ORM\EntityManager;
  */
 class Searches
 {
-
     private $em;
 
-    public function __construct(EntityManager $em) {
+    public function __construct(EntityManager $em)
+    {
         $this->em = $em;
     }
 
-    public function getLatest($site) {
+    public function getLatest($site)
+    {
         $em = $this->em;
         $site = $em->getRepository('TruckeeProjectmanaBundle:Center')->find($site);
         $maxDate = $em->createQuery('SELECT MAX(c.contactDate) FROM '
-                        . 'TruckeeProjectmanaBundle:Contact c WHERE c.center = :site')
-                ->setParameter('site', $site)
-                ->getSingleScalarResult();
+                . 'TruckeeProjectmanaBundle:Contact c WHERE c.center = :site')
+            ->setParameter('site', $site)
+            ->getSingleScalarResult();
         $contacts = $em->createQuery('SELECT m.sname, m.fname, m.dob, h.id, d.contactDesc FROM TruckeeProjectmanaBundle:Member m '
-                        . 'JOIN TruckeeProjectmanaBundle:Household h WITH m = h.head '
-                        . 'JOIN TruckeeProjectmanaBundle:Contact c WITH c.household = h '
-                        . 'JOIN TruckeeProjectmanaBundle:ContactDesc d WITH c.contactDesc = d '
-                        . 'WHERE c.center = :site AND c.contactDate = :date '
-                        . 'ORDER BY m.sname, m.fname')
-                ->setParameters(['site' => $site, 'date' => $maxDate])
-                ->getResult();
+                . 'JOIN TruckeeProjectmanaBundle:Household h WITH m = h.head '
+                . 'JOIN TruckeeProjectmanaBundle:Contact c WITH c.household = h '
+                . 'JOIN TruckeeProjectmanaBundle:ContactDesc d WITH c.contactDesc = d '
+                . 'WHERE c.center = :site AND c.contactDate = :date '
+                . 'ORDER BY m.sname, m.fname')
+            ->setParameters(['site' => $site, 'date' => $maxDate])
+            ->getResult();
 
         return array(
             'contacts' => $contacts,
@@ -56,7 +56,8 @@ class Searches
      *
      * @return array
      */
-    public function getMembers($qtext) {
+    public function getMembers($qtext)
+    {
         $string = trim($qtext);
         $length = strpos($qtext, ' ');
         if (empty($length)) {
@@ -75,4 +76,30 @@ class Searches
         return $found;
     }
 
+    public function getHeadsFYToDate($site)
+    {
+        $date = new \DateTime();
+        $year = date_format($date, 'Y');
+        $month = date_format($date, 'n');
+        $fy = ($month < 7) ? $year : $year + 1;
+        $startDate = $fy - 1 . '-07-01';
+        $endDate = $fy . '-06-30';
+
+        $em = $this->em;
+        $loc = $em->getRepository('TruckeeProjectmanaBundle:Center')->find($site);
+        $contacts = $em->createQuery('SELECT DISTINCT m.sname, m.fname, m.dob, h.id FROM TruckeeProjectmanaBundle:Member m '
+                . 'JOIN TruckeeProjectmanaBundle:Household h WITH m = h.head '
+                . 'JOIN TruckeeProjectmanaBundle:Contact c WITH c.household = h '
+                . 'WHERE c.center = :site AND c.contactDate >= :startDate '
+                . 'AND c.contactDate <= :endDate '
+                . 'ORDER BY m.sname, m.fname')
+            ->setParameters([
+                'site' => $loc,
+                'startDate' => $startDate,
+                'endDate' => $endDate,
+            ])
+            ->getResult();
+
+        return $contacts;
+    }
 }
